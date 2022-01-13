9f652cab80780e971da9852d9cbea0fb2a190907
PebriKurniaSandi <fksandi07@gmail.com>
2022-01-13 14:44:10 +0700

    consume

---
+++ index.js
@@ -0,0 +1,52 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+  }
+  
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
+  }
+  
+  $(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#dataTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+  });
+  
+  $("#dataTable").ready(function () {
+    var tabel = document.getElementById("dataTable")
+    getAll().then(response => {
+        console.log(response)
+        for(var i = 0; i <response.length; i++){
+            const tr = tabel.insertRow()
+            const td1 = tr.insertCell();
+            const td2 = tr.insertCell();
+            const td3 = tr.insertCell();
+            const td4 = tr.insertCell();
+            const td5 = tr.insertCell();
+            const td6 = tr.insertCell();
+            console.log(response[i])
+            td1.innerHTML = response[i].idsiswa
+            td2.innerHTML = response[i].nama
+            td3.innerHTML = response[i].kelas
+            td4.innerHTML = response[i].nis
+            td5.innerHTML = response[i].alamat
+            td6.innerHTML = `<div class ="text-center align-middler">
+            <a class="btn btn-warning btn-sm text-white" href="edit_data.html?nik=${response[i].idsiswa}">Edit</a>
+            <button type ="button" class="btn btn-danger btn-sm" onclick="del(${response[i].idsiswa});">Delete</button>
+            </div>`
+            }
+        }
+    )
+  });
